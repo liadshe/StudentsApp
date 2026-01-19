53ee531f5a5bac70d238fe71b3e7b9b280b4989b
nunusha712 <noa.hadad712@gmail.com>
2026-01-19 14:52:26 +0200

    student recycler view

---
+++ app/src/main/java/com/example/studentsapp/StudentRecyclerViewActivity.kt
@@ -0,0 +1,45 @@
+package com.example.studentsapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.studentsapp.databinding.ActivityStudentsRecyclerViewBinding
+import com.example.studentsapp.models.Model
+import com.example.studentsapp.models.Student
+
+class StudentRecyclerViewActivity : AppCompatActivity() {
+    var binding : ActivityStudentsRecyclerViewBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityStudentsRecyclerViewBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val layout = LinearLayoutManager(this)
+        binding?.recyclerView?.layoutManager = layout
+        binding?.recyclerView?.setHasFixedSize(true)
+
+        val adapter = StudentsAdapter(Model.shared.students)
+        adapter.listener = object: onItemClickListener {
+            override fun onItemClick(position: Int)
+            {
+                Log.d("StudentsRecyclerViewActivity", "Button clicked!")
+            }
+
+            override fun onStudentItemClick(student: Student) {
+                Log.d("StudentsRecyclerViewActivity", "Student clicked!")
+            }
+
+        }
+        binding?.recyclerView?.adapter = adapter
+    }
+}
---
+++ app/src/main/java/com/example/studentsapp/models/Student.kt
@@ -0,0 +1,9 @@
+package com.example.studentsapp.models
+
+import android.R
+
+data class Student(
+    val name: String?,
+    val id: String?,
+    val pic: Int?,
+    var isPresent: Boolean)
